7608477a2a14d010ffe95202a4c1d16605579a30
Chris Milson <chris.w.milson@gmail.com>
2020-08-07 09:37:47 +0900

    Wrap code editor for styles

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react'
 import AceEditor from 'react-ace'
 import 'ace-builds/src-noconflict/mode-latex'
 import 'ace-builds/src-noconflict/theme-github'
+import './Editor.css'
 
 const Editor: React.FC<{
   code?: string
@@ -27,21 +28,23 @@ const Editor: React.FC<{
   }, [ref])
 
   return (
-    <div ref={ref} className="Editor">
-      <AceEditor
-        name="latex-input"
-        placeholder="Enter LaTeX math code here..."
-        theme="github"
-        mode="latex"
-        onChange={onChange}
-        fontSize={14}
-        showPrintMargin={false}
-        showGutter={false}
-        highlightActiveLine={false}
-        value={code}
-        width={`${width - 20}px`}
-        height={`${height - 20}px`}
-      />
+    <div className="Editor">
+      <div ref={ref} className="Wrapper">
+        <AceEditor
+          name="latex-input"
+          placeholder="Enter LaTeX math code here..."
+          theme="github"
+          mode="latex"
+          onChange={onChange}
+          fontSize={14}
+          showPrintMargin={false}
+          showGutter={false}
+          highlightActiveLine={false}
+          value={code}
+          width={`${width}px`}
+          height={`${height}px`}
+        />
+      </div>
     </div>
   )
 }
